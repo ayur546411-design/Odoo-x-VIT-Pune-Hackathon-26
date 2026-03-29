39c2e90a7f8f2f11da2258726da3c38bce6e27aa
Ayush kumar <ayur546411@gmail.com>
2026-03-29 16:51:01 +0530

    Delete odoo directory

--- odoo/src/api/workflow.js
+++
@@ -1,9 +0,0 @@
-import api from './axios'
-
-export const getWorkflows = () => api.get('/workflows')
-
-export const createWorkflow = (data) => api.post('/workflows', data)
-
-export const updateWorkflow = (id, data) => api.put(`/workflows/${id}`, data)
-
-export const deleteWorkflow = (id) => api.delete(`/workflows/${id}`)
